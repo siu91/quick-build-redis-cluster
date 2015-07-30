aa112f30b058716d05fbd6cf552b35ef90d9f5b6
gshiwen <gshiwen@gmail.com>
2015-07-30 22:59:49 +0800

    Update build-conf.sh

--- bin/build-conf.sh
+++ bin/build-conf.sh
@@ -5,10 +5,6 @@
 # Author:gongice
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-########
-#COMMON#
-########
-
 this="${BASH_SOURCE-$0}"
 common_bin=$(cd -P -- "$(dirname -- "$this")" && pwd -P)
 parent_dir=`cd "$common_bin/../" >/dev/null; pwd`
